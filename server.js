6c9e057822e0d1437d41e851a5020f7b47e2e43b
Dilki687 <chamodidilki44@gmail.com>
2024-11-29 13:03:15 +0530

    vercel.json

--- server.js
+++ server.js
@@ -3,20 +3,25 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const dbConfig = require('./db')
 const dotenv = require("dotenv");
-const passport = require("passport");
+// const passport = require("passport");
 const orderRoutes = require('./routes/orderRoute');
 const authRoutes = require("./routes/auth"); 
 
 dotenv.config();
-require("./config/passport");
+// require("./config/passport");
 
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://shion-green-frontend.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 app.use(bodyParser.json());
 app.use(express.json()); // Ensure JSON parsing is supported
-app.use(passport.initialize()); // Initialize passport
 
 // Routes
 
